2c031a9f1ee03402a7c716c34ed51712727be6bc
AIVIETNAM-AIO-ThuThao <040401240126@st.buh.edu.vn>
2026-06-08 17:43:23 +0700

    ukie

--- b29.py
+++ b29.py
@@ -1,9 +1,9 @@
-'''Bài 29. Tìm số ngày của năm N, biết rằng năm nhuận là năm chia hết cho 400 hoặc chia hết cho 4 
+'''Bài 29. Tìm số ngày của năm N, biết rằng năm nhuận là năm chia hết cho 400; hoặc chia hết cho 4 
 nhưng không chia hết cho 100'''
 while True:
     try:
         year = int(input("Hãy nhập vào số năm: "))
-        if year % 400 == 0 or year % 4 == 0:
+        if (year % 400 == 0) or (year % 4 == 0 and year % 100 !=0):
             print(f"Số ngày của năm {year} là 366 ngày")
         else: 
             print(f"Số ngày của năm {year} là 365 ngày")
